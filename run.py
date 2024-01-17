1391831564274233fc6befbeabc5a4a06a6413e9
yakus <nikijakushev@yandex.ru>
2024-01-17 23:31:34 +0300

    добавил карту отображение игрока не настроил

--- run.py
+++ run.py
@@ -1,4 +1,27 @@
 import pygame
+import pytmx
+
+
+class Map:
+    def __init__(self, pl):
+        self.map = pytmx.load_pygame('map.tmx')
+        self.height = self.map.height
+        self.width = self.map.width
+        self.tile_size = self.map.tilewidth
+        self.pl = pl
+
+    def render(self, screen):
+        for i in [0, 1, 2]:
+            for y in range(self.height):
+                for x in range(self.width):
+                    image = self.map.get_tile_image(x, y, i)
+                    if image:
+                        screen.blit(image, (x * self.tile_size, y * self.tile_size))
+
+    def get_tile_id(self, pos):
+        return self.map.tiledgidmap[self.map.get_tile_gid(*pos, 0)]
+    def get_free(self, pos):
+        return self.get_tile_id(pos) in self.pl
 
 
 
@@ -10,6 +33,7 @@ class Player(pygame.sprite.Sprite):
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
+
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -34,13 +58,14 @@ class Player(pygame.sprite.Sprite):
 
 
 clock = pygame.time.Clock()
-size = height, width = 1280, 720
+size = height, width = 1920, 800
 screen = pygame.display.set_mode(size)
 player_sprites = pygame.sprite.Group()
 see_sprites = pygame.sprite.Group()
 evil_sprites = pygame.sprite.Group()
 pl = Player(pygame.image.load("datafiles/anim.png"), 5, 1, 10, 400)
 running = True
+mapp = Map(97)
 while running:
     for i in pygame.event.get():
         pass
@@ -50,6 +75,9 @@ while running:
     if key[pygame.K_LEFT]:
         pl.update("l")
     screen.fill(pygame.Color("black"))
+
+    # Update the map
+    mapp.render(screen)
     # Update the animations
     player_sprites.draw(screen)
     pygame.display.flip()
