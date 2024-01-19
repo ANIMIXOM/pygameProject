19adddb3590d8335a1916910410cf6a31dc3753d
Nikita1451 <118666275+Nikita1451@users.noreply.github.com>
2024-01-20 01:09:33 +0300

    Delete anim_stay.tsx

--- anim_stay.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="anim_stay" tilewidth="52" tileheight="88" spacing="20" tilecount="1" columns="1">
- <image source="../OneDrive/Рабочий стол/anim_stay.png" width="52" height="88"/>
-</tileset>
