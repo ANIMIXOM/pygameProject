dd6fb37f08e9f2e12dfd239bff11691bb4993d5f
ANIMIXOM <maximshlyamnev@yandex.ru>
2024-01-19 18:53:32 +0300

    by ANIMIXOM - торговец

--- run.py
+++ run.py
@@ -1,19 +1,69 @@
 import pygame
 
 pygame.init()
+gui_torg = False
 
 
-class gui():
+class gui_torg(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.gui_sell = pygame.image.load("datafiles/sell.png")
+        self.rect_sell = self.gui_sell.get_rect()
+
+    def update(self):
+        screen.blit(self.gui_sell, (200, 200))
+        key = pygame.key.get_pressed()
+        if key[pygame.K_g]:
+            if inventory["wearpon"] > 0:
+                inventory["wearpon"] -= 1
+                inventory["rupis"] += 1
+            else:
+                print("У вас нету товара")
+        if key[pygame.K_h]:
+            if inventory["rupis"] >= 3:
+                inventory["rupis"] -= 3
+                inventory["fire_cristal"] += 1
+            else:
+                print("Нема деняг")
+
+
+class torg(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(see_sprites)
+        self.image = pygame.image.load("datafiles/torg.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 500
+        self.rect.y = 300
+
+    def update(self):
+        if pygame.sprite.spritecollide(self, player_sprites, dokill=False):
+            global fand_torg
+            fand_torg = True
+            pl.rect.x += 200
+        screen.blit(self.image, (700, 280))
+
+
+maps = "map0"
+
+
+class gui:
     def __init__(self):
         self.font = pygame.font.Font("datafiles/Lightman.ttf", 20)
         self.text_surface = self.font.render("99", True, (255, 255, 255))
         self.healim = pygame.image.load("datafiles/health.png")
         self.healim = pygame.transform.scale(self.healim, (50, 50))
+        self.summ = str(inventory["rupis"])
+        self.rup = self.font.render(self.summ, True, (20, 255, 20))
 
     def update(self):
         screen.blit(self.healim, (10, 10))
         screen.blit(self.text_surface, (20, 20))
-        self.text_surface = self.text_surface = self.font.render(str(pl.hp), True, (255, 255, 255))
+        screen.blit(self.rup, (100, 20))
+        self.summ = str(inventory["rupis"])
+        self.rup = self.font.render(self.summ, True, (20, 255, 20))
+        self.text_surface = self.text_surface = self.font.render(
+            str(pl.hp), True, (255, 255, 255)
+        )
 
 
 class Evil(pygame.sprite.Sprite):
@@ -56,13 +106,15 @@ class enger_spire(pygame.sprite.Sprite):
         self.rect = self.rect.move(x, y)
 
     def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
+        self.rect = pygame.Rect(
+            0, 0, sheet.get_width() // columns, sheet.get_height() // rows
+        )
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
+                self.frames.append(
+                    sheet.subsurface(pygame.Rect(frame_location, self.rect.size))
+                )
 
     def update(self):
         self.cur_frames += 1
@@ -84,22 +136,26 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.rect.move(x, y)
 
     def cut_sheet_hod(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
+        self.rect = pygame.Rect(
+            0, 0, sheet.get_width() // columns, sheet.get_height() // rows
+        )
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames_hod.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
+                self.frames_hod.append(
+                    sheet.subsurface(pygame.Rect(frame_location, self.rect.size))
+                )
 
     def cut_sheet_attack(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
+        self.rect = pygame.Rect(
+            0, 0, sheet.get_width() // columns, sheet.get_height() // rows
+        )
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames_at.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
+                self.frames_at.append(
+                    sheet.subsurface(pygame.Rect(frame_location, self.rect.size))
+                )
 
     def update_hod(self, n):
         self.cur_frame = self.cur_frame + 1
@@ -127,9 +183,16 @@ class Player(pygame.sprite.Sprite):
             self.cur_frame = 0
 
 
-inventory = {"fire_cristal": 2, "el_cristal": 2, "healme5": 3, "healme20": 10, "healme100": 1,
-             "wearpon": 0, "rupis": 0}
-
+inventory = {
+    "fire_cristal": 10,
+    "el_cristal": 2,
+    "healme5": 3,
+    "healme20": 10,
+    "healme100": 1,
+    "wearpon": 20,
+    "rupis": 20,
+}
+fand_torg = False
 otr = False
 clock = pygame.time.Clock()
 size = height, width = 1280, 720
@@ -142,10 +205,13 @@ pl = Player(pygame.image.load("datafiles/anim.png"), 5, 1, 10, 500)
 running = True
 attack = [False, 0]
 attack_cr = [False, 0]
-Evil(player_sprites, 500, 500, pygame.image.load("datafiles/evil1.png"), 30)
 gui = gui()
+Torg = torg()
+torg_in_map = True
+gui_t = gui_torg()
 sound_hod = pygame.mixer.Sound("datafiles/soundh.mp3")
 fire_son = pygame.mixer.Sound("datafiles/fire_son.mp3")
+mous = None
 while running:
     try:
         for event in pygame.event.get():
@@ -159,12 +225,18 @@ while running:
                 pl.update_attack()
                 for i in evil_sprites:
                     if abs(i.rect.x - pl.rect.x) <= 60:
-                        i.hp -= 2
+                        i.hp -= 5
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
                     if inventory["fire_cristal"] != 0:
                         inventory["fire_cristal"] -= 1
-                        enger_spire(pygame.image.load("datafiles/anim_fire.png"), 3, 1, pl.rect.x - 20, pl.rect.y - 10)
+                        enger_spire(
+                            pygame.image.load("datafiles/anim_fire.png"),
+                            3,
+                            1,
+                            pl.rect.x - 20,
+                            pl.rect.y - 10,
+                        )
                         attack_cr = [True, 0]
                         fire_son.play()
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -204,6 +276,10 @@ while running:
         evil_sprites.update()
         player_sprites.draw(screen)
         gui.update()
+        if torg_in_map:
+            Torg.update()
+        if fand_torg:
+            gui_t.update()
         pygame.display.flip()
         clock.tick(10)
     except IndexError:
