223957b681eb59416bc41916fc9bd2d874ea4371
Nikita1451 <118666275+Nikita1451@users.noreply.github.com>
2024-01-20 01:09:41 +0300

    Delete ztwPZOI.tsx

--- ztwPZOI.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="ztwPZOI" tilewidth="32" tileheight="32" tilecount="2544" columns="8">
- <grid orientation="orthogonal" width="20" height="20"/>
- <image source="../Downloads/ztwPZOI.png" width="256" height="4256"/>
-</tileset>
